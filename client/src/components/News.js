6d37a9b4e5fab2962c7c53c0502fb81803b27530
barisyilirim06 <yildrmbaris@gmail.com>
2020-08-14 18:03:31 +0300

    load more added

--- client/src/components/News.js
+++ client/src/components/News.js
@@ -14,36 +14,71 @@ function News() {
         { _id: 6, name: "LIFE" },
         { _id: 7, name: "SHOPIPING" }
     ]
-    const X=0;
-    const Y=6;
+    const X = 0;
+    const Y = 6;
     const [Products, setProducts] = useState([])
+    const [Skip, setSkip] = useState(0)
+    const [PostSize, setPostSize] = useState()
+    const Limit = 10
 
-    
+
     useEffect(() => {
-        Axios.post('/api/product/getProducts')
-        .then(response => {
-            if (response.data.success) {
-                setProducts(response.data.products)
-            } else {
-                alert('Failed to fectch product datas')
-            }
-        })
+
+        const variables = {
+            skip: Skip,
+            limit: Limit,
+        }
+
+        getProducts(variables)
+
     }, [])
 
-  
+    const getProducts = (variables) => {
+        Axios.post('/api/product/getProducts', variables)
+            .then(response => {
+                if (response.data.success) {
+                    if (variables.loadMore) {
+                        setProducts([...Products, ...response.data.products])
+                    } else {
+                        setProducts(response.data.products)
+                    }
+                    setPostSize(response.data.postSize)
+                } else {
+                    alert('Failed to fectch product datas')
+                }
+            })
+    }
+
+    const onLoadMore = () => {
+        let skip = Skip + Limit;
+
+        const variables = {
+            skip: skip,
+            limit: Limit,
+            loadMore: true,
+        }
+        getProducts(variables)
+        setSkip(skip)
+    }
+
     return (
 
         <div className="container">
             <div className="column col-8 col-s-12 left1">
                 <h4><Title title="LATEST NEWS" /></h4>
                 <div >
-                    <RenderNews 
-                    Products={Products}
-                    X={1}
-                    Y={100}
-                    categories={categories}
+                    <RenderNews
+                        Products={Products}
+                        X={1}
+                        Y={100}
+                        categories={categories}
                     />
                 </div>
+                {PostSize >= Limit &&
+                <div style={{ display: 'flex', justifyContent: 'center' }}>
+                    <button onClick={onLoadMore}>Load More</button>
+                </div>
+            }
             </div>
         </div>
     )
