5e8c55b03fc0924c7fae474a8fbd0e6961628881
barisyilirim06 <yildrmbaris@gmail.com>
2020-09-03 17:45:55 +0300

    responsive design

--- client/src/components/utils/RenderVideoPage.js
+++ client/src/components/utils/RenderVideoPage.js
@@ -1,9 +1,18 @@
 import React, { useState, useEffect, useRef } from 'react'
 import Title from '../Title'
 import $ from 'jquery';
+import { useMediaQuery } from 'react-responsive'
+
 
 function RenderVideoPage() {
 
+    const isBigEnough = useMediaQuery({
+        query: '(min-width: 780px)'
+    })
+    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1000px)' })
+    const isComputer = useMediaQuery({
+        query: '(min-width: 1000px)'
+    })
    const [VideoSeen, setVideoSeen] = useState(0)
     var $window = $(window);
     var $videoWrap = $('.video-wrap');
@@ -15,20 +24,37 @@ function RenderVideoPage() {
         var windowScrollTop = $window.scrollTop();
         // var videoBottom = videoHeight + $video.offset().left;
         // 1400 2600
-        if (windowScrollTop >1400){
-            setVideoSeen(1)
+        if(isComputer ){
+            if (windowScrollTop >1400){
+                setVideoSeen(1)
+            }
+            if (windowScrollTop > 2600) {
+                $videoWrap.height(videoHeight);
+                $video.addClass('stuck');
+                
+            } else if (windowScrollTop >1400 && windowScrollTop <2600){
+                $videoWrap.height('auto');
+                $video.removeClass('stuck');
+            }  else if (VideoSeen === 1 && windowScrollTop < 1400) {
+                $videoWrap.height(videoHeight);
+                $video.addClass('stuck');
+            } 
+        } else if(isBigEnough && isTabletOrMobile) {
+            if (windowScrollTop >2000){
+                setVideoSeen(1)
+            }
+            if (windowScrollTop > 3200) {
+                $videoWrap.height(videoHeight);
+                $video.addClass('stuck');
+                
+            } else if (windowScrollTop >2000 && windowScrollTop <3200){
+                $videoWrap.height('auto');
+                $video.removeClass('stuck');
+            }  else if (VideoSeen === 1 && windowScrollTop < 2000) {
+                $videoWrap.height(videoHeight);
+                $video.addClass('stuck');
+            } 
         }
-        if (windowScrollTop > 2600) {
-            $videoWrap.height(videoHeight);
-            $video.addClass('stuck');
-            
-        } else if (windowScrollTop >1400 && windowScrollTop <2600){
-            $videoWrap.height('auto');
-            $video.removeClass('stuck');
-        }  else if (VideoSeen === 1 && windowScrollTop < 1400) {
-            $videoWrap.height(videoHeight);
-            $video.addClass('stuck');
-        } 
     });
 
 
@@ -36,11 +62,15 @@ function RenderVideoPage() {
         <div className="videos">
             <div className="videos-container" >
                 <h4><Title title="TOP VIDEOS" color="#0dbe98" /></h4>
-                <div class="video-wrap">
+                {isBigEnough ? <div class="video-wrap">
                     <div class="video">
-                        <iframe width='765px' height='433px' id="video1" src="https://www.youtube.com/embed/_YgqROO4BYw?autoplay=1&mute=1&enablejsapi=1" frameborder="0" gesture="media" allowfullscreen></iframe>
+                        <iframe width='765px' height='433px' src="https://www.youtube.com/embed/_YgqROO4BYw?autoplay=1&mute=1&enablejsapi=1" frameborder="0" gesture="media" allowfullscreen></iframe>
                     </div>
+                </div> : <div className="video-phone">
+                <iframe className="video-phone" width="100%" height="300px" src="https://www.youtube.com/embed/_YgqROO4BYw?autoplay=1&mute=1&enablejsapi=1" frameborder="0" gesture="media" allowfullscreen></iframe>
                 </div>
+                }
+                
             </div>
         </div>
     )
