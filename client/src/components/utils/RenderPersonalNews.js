400bb64e2a36f2938591e2b8ca55e31c0b803a21
barisyilirim06 <yildrmbaris@gmail.com>
2020-09-03 15:20:41 +0300

    responsive design

--- client/src/components/utils/RenderPersonalNews.js
+++ client/src/components/utils/RenderPersonalNews.js
@@ -8,18 +8,23 @@ function RenderFeatured(props) {
     const Products = props.Products;
     const categories = props.categories
     return (Products.slice(X, Y).map((product) => {
-        return <div>
+        return <div >
             <div className="personalbox">
-                <div className="featured">
-                    <Link to={`/product/${product._id}`}><img src={product.imageshorizontal} alt={product.imageshorizontal} /></Link>
-                    <br/>
-                    <br/>
-                    <div className="personalcategory"><Link to={`/news/${categories[product.categories].name.toLowerCase()}`}>{categories[product.categories].name}</Link></div>
-                    <p className="personalheader"><Link to={`/product/${product._id}`}>{product.title}</Link></p>
-                    <div className="personalmain">{product.subtitle}</div>
-                </div>
+                    <div className="row2">
+                        <div>
+                            <Link to={`/product/${product._id}`}><img src={product.imageshorizontal} alt={product.imageshorizontal} /></Link>
+                        </div>
+                        <div>
+                            <div className="personalcategory"><Link to={`/news/${categories[product.categories].name.toLowerCase()}`}>{categories[product.categories].name}</Link></div>
+                            <p className="personalheader"><Link to={`/product/${product._id}`}>{product.title}</Link></p>
+                            <div className="personalmain">{product.subtitle}</div>
+                        </div>
+                        
+                    </div>
+                    
             </div>
-            <hr/>
+            <br/>
+                            <hr />
         </div>
 
     }))
